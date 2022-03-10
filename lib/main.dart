87ff364f6f1fc8a07bf8b2b50aa6145798cf6eb6
Kana <17823kana@gmail.com>
2022-03-10 12:41:26 +0900

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/pages/intermediate_page.dart';
 import 'package:flutter_practice/sample_answers/beginner_page.dart';
 import 'package:flutter_practice/sample_answers/preliminary_page.dart';
 import 'pages/beginner_page.dart';
@@ -14,11 +15,11 @@ class PracticeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.blue,
       ),
-      //debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       // ここで最初に表示する画面を変更
-      home: const AnswerBeginnerPage(),
+      home: const IntermediatePage(),
       // home: const BeginnerPage(),
     );
   }
--- lib/pages/intermediate_page.dart
+++ lib/pages/intermediate_page.dart
@@ -8,6 +8,29 @@ class IntermediatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ここを実装
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: _appBar(), // 上のバー
+    );
+  }
+
+  // 上のAppバー
+  AppBar _appBar() {
+    return AppBar(
+      title: const Text(
+        "Flutter Practice2",
+        style: TextStyle(color: Colors.white),
+      ),
+      // AppBarの右端のボタン（リストなので複数可）
+      actions: [
+        IconButton(
+          icon: const Icon(
+            Icons.more_horiz,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
   }
 }
